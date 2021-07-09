7e6054acee1c5d2388ce3ac82739cdcf0a733dd5
Tommy Knight <78544868+Tommy-Knight@users.noreply.github.com>
2021-07-09 08:56:04 +0100

    serverstart

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import express from "express";
+import cors from "cors";
+import listEndpoints from "express-list-endpoints";
+import mongoose from "mongoose";
+import dotenv from "dotenv"
+
+
+dotenv.config()
+const server = express();
+server.use(cors());
+server.use(express.json())
+const port = process.env.PORT || 3420;
+
+// ><><><><: MIDDLEWARES :><><><>< \\
+
+
+// ><><><><: ROUTES :><><><>< \\
+
+// server.use("/accomodation", accomodationRouter);
+// server.use("/destination", destinationRouter);
+
+// ><><><><: ERROR MIDDLEWARES :><><><>< \\
+
+
+console.table(listEndpoints(server));
+
+// ><><><><: MONGO TIME :><><><>< \\
+mongoose
+	.connect(
+		process.env.MONGOOSE_CONNECTION,
+		 { unifiedTopology: true }
+	)
+	.then(() => {
+		console.log("Connected to mongo");
+		server.listen(port, () => {
+			console.log(list(app));
+			console.log("Server listening on port " + port + " 🍌");
+		});
+	});
+
